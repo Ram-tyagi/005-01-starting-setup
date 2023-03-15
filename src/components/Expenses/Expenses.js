b8c0604f940f8776e2d7a554dea32429bb3260cc
Ram-tyagi <ramtyagi04@gmail.com>
2023-03-15 11:29:36 +0530

    AdReactChanllenge

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,32 @@
+import React,{useState} from 'react';
+import Card from '../UI/Card';
+import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpenseFilter";
+
+import './Expenses.css';
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  const filteredExpenses=props.items.filter(expense=>{
+    return expense.date.fetFullYear.toString()===filteredYear;
+  })
+  return (
+    <Card className="expenses">
+      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+      {filteredExpenses.map((expense)=>(
+      <ExpenseItem
+       key={expense.id} 
+       title={expense.title} 
+       amount={expense.amount} 
+       date={expense.date}/>
+       ))}
+
+    </Card>
+  );
+}
+
+export default Expenses;
